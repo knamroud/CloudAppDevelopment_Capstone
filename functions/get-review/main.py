5299156f94bfc05b0e985ee994e11f87ec753a91
Kirelos Namroud <namroudkirelo04@gmail.com>
2023-11-27 22:09:05 +0100

    Remove sample functions and add get-review

---
+++ functions/get-review/main.py
@@ -0,0 +1,23 @@
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+
+def main(param_dict):
+    authenticator = IAMAuthenticator(param_dict["IAM_API_KEY"])
+    client = CloudantV1(authenticator=authenticator)
+    client.set_service_url(param_dict["COUCH_URL"])
+    response = client.post_find(
+        db='reviews',
+        selector={'dealership': {'$eq': int(param_dict['id'])}},
+    ).get_result()
+    try: 
+        result= {
+            'headers': {'Content-Type':'application/json'}, 
+            'body': {'data':response} 
+            }        
+        return result
+    except:  
+        return { 
+            'statusCode': 404, 
+            'message': 'Something went wrong'
+            }
