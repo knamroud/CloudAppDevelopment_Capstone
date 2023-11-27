3a493ced5b61d57d9f7f64e3cd5a107138a4d1a9
Kirelos Namroud <namroudkirelo04@gmail.com>
2023-11-27 22:30:09 +0100

    Add add-review

---
+++ functions/add-review/main.py
@@ -0,0 +1,26 @@
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+
+def main(param_dict):
+    authenticator = IAMAuthenticator(param_dict["IAM_API_KEY"])
+    client = CloudantV1(authenticator=authenticator)
+    client.set_service_url(param_dict["COUCH_URL"])
+
+    new_review = client.post_document(db="reviews", document=param_dict["review"])   
+    
+    if new_review.exists():
+        result = {
+            "headers": {"Content-Type": "application/json"},
+            "body": {"message": "Review posted successfully."}
+        }
+    
+        print(new_review)
+        return result
+        
+    else: 
+        error_json = {
+            "statusCode": 500,
+            "message": "Could not post review due to server error."
+        }
+        return error_json
